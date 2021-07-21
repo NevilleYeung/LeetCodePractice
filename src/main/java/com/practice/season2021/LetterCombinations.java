b248c722feb567979becaccfc0176552ca830d12
NevilleYeung <wj89757@126.com>
2021-07-21 22:21:45 +0800

    LeetCode练习：DFS
    46. 全排列

---
+++ src/main/java/com/practice/season2021/LetterCombinations.java
@@ -0,0 +1,9 @@
+package com.practice.season2021;
+
+import java.util.List;
+
+public class LetterCombinations {
+    public List<String> letterCombinations(String digits) {
+
+    }
+}
--- src/main/java/com/practice/season2021/Permute.java
+++ src/main/java/com/practice/season2021/Permute.java
@@ -8,6 +8,26 @@ import java.util.*;
  * https://leetcode-cn.com/problems/permutations/
  */
 public class Permute {
+    /*
+    给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+
+    示例 1：
+    输入：nums = [1,2,3]
+    输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+    示例 2：
+    输入：nums = [0,1]
+    输出：[[0,1],[1,0]]
+
+    示例 3：
+    输入：nums = [1]
+    输出：[[1]]
+     
+    提示：
+    1 <= nums.length <= 6
+    -10 <= nums[i] <= 10
+    nums 中的所有整数 互不相同
+     */
     public List<List<Integer>> permute(int[] nums) {
         boolean[] seen = new boolean[nums.length];
         List<List<Integer>> ans = new ArrayList<>();
